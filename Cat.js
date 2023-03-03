115fa8d139fb3c08a56974b05d9ae9db23f19714
chuong <ngchuongwork@gmail.com>
2023-03-03 21:04:20 +0700

    add function cat die

--- Cat.js
+++ Cat.js
@@ -1,5 +1,6 @@
 function cat(name){
 	this.name = name;
+    this.die = false;
 	this.stomach =[];
 }
 function eat(mouse){
@@ -7,5 +8,8 @@ function eat(mouse){
 }
 function drink(water){
     this.stomach.push(water);
+}
+function die(){
+    this.die= true;
 }
 module.export= cat;
